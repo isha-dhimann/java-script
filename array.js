ab92145bb4cc5b4899d0d9c71d8a6ccfb56dd70a
Isha-dhimann <ishadhiman2681@gmail.com>
2025-07-30 21:45:58 +0530

    30-7-25

--- array.js
+++ array.js
@@ -15,7 +15,6 @@
 //     console.log("isha");
 // }, 3000);
 
-
 // ARRAYS(collections of elements,char,numbers.....its denoted by the square brackets[]
 
 // step -1
@@ -28,15 +27,14 @@
 // let ar = [10,20,30,40,50,60,70]
 // console.log(ar[3])
 
-
 // step-2
 
 // let ar=new Array(6);
 // ar[0]=20;
 // console.log(ar);
 
-
-// let arr=Array.of(1,2,3,4,5);
+// let arr=A
+// rray.of(1,2,3,4,5);
 // console.log(arr);
 
 // step -3
@@ -44,7 +42,6 @@
 // let str = "ishaa"
 // let ar= Array.from(str)
 // console.log(ar);
-
 
 // let str = "my name is isha"
 // let ar= Array.from(str)
@@ -66,7 +63,6 @@
 // let ar2=[1000,2000,3000,4000]
 // let ar =[ar1,ar2]
 // console.log(ar);
-
 
 // VARIOUS FUNCTION USED IN ARRAYS
 // PUSH
@@ -118,7 +114,6 @@
 // let ar = ar1.concat (ar2)
 // console.log(ar);
 
-
 // FOREACH(har ek member k liye call)
 
 // step-1
@@ -165,17 +160,226 @@
 // step - 2
 
 // let ar = [10,20,30,50]
-// let newArr = ar.map((item) => item * 3) 
+// let newArr = ar.map((item) => item * 3)
 // console.log(newArr);
 
 // let ar = [10,20,30,50]
-// let newArr = ar.map((item) => item * 10) 
+// let newArr = ar.map((item) => item * 10)
 // console.log(newArr);
 
 // let ar = [10,20,30,50]
-// let newArr = ar.map((item) => "ishaanchlas") 
+// let newArr = ar.map((item) => "ishaanchlas")
 // console.log(newArr);
 
 // let ar = [10,20,30,50]
-// let newArr = ar.map((item) => "Rainy Season") 
+// let newArr = ar.map((item) => "Rainy Season")
 // console.log(newArr);
+
+// Extra questions............
+
+// let marks = [10,30,60,90,80,70]
+// console.log(marks[0]);
+
+// let arr = [10, 20, 50, 60];
+// let newarr = arr.push(1000,67);
+// console.log(arr);
+
+
+// let arr = [10, 20, 50, 60];
+// let newarr = arr.push(1000,67);
+// console.log(arr);
+// console.log(newarr);
+
+// let arr = [20,40,60,80]
+// arr.pop()
+// console.log(arr);
+
+// let arr = [20,40,60,80]
+// arr.shift()
+// console.log(arr);
+
+// let arr = [20,40,60,80]
+// arr.unshift(100)
+// console.log(arr);
+
+
+// SPLICE (bich se value are remove)
+// let arr = [1,2,3,4,5]
+// arr.splice(2,1)
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6]
+// arr.splice(2,3)
+// console.log(arr);
+
+// let arr = ["isha" , "muskan" , 10,20,30,70]
+// arr.splice(1,4)
+// console.log(arr);
+
+// let arr = ["isha" , "muskan" , 10,20,30,70,"soma"]
+// arr.splice(1,6)
+// console.log(arr);
+
+// SLICE (main array are don't change its return the new array)
+
+// let arr = [20,30,40,50]
+// let  newarr = arr.slice(0,3)
+// console.log(newarr);
+// console.log(arr);
+
+// let arr = [1,2,3,4,5];
+// let  newarr = arr.slice(0,3);
+// console.log(newarr);
+
+// let arr = ["isha" , "muskan" , 10,20,30,70]
+//  let newarr=arr.slice(1,4)
+// console.log(newarr);
+
+// REVERSE (only change main array )
+
+// let arr=[10,20,30,40,50]
+// arr.reverse()
+// console.log(arr);
+
+// let arr = ["isha" , "muskan" , 10,20,30,70]
+// arr.reverse()
+// console.log(arr);
+
+// SORT (orders = acending , decending)
+
+// let arr = [11,56,100,3,7]
+// arr.sort(function(x,y){
+//     return x - y 
+// }
+// );
+// console.log(arr);
+
+// let arr = [11,56,100,3,7]
+// arr.sort(function(x,y){
+//     return y - x 
+// }
+// );
+// console.log(arr);
+
+// let arr = [11,56,100,3,7,"isha"]
+// arr.sort(function(x,y){
+//     return y - x 
+// }
+// );
+// console.log(arr);
+
+
+// MAP , FILTER , REDUCE , FOREACH
+
+// let arr = [11,62,3,4,25]
+// arr.forEach(function(val){
+// console.log(val)
+// }
+// );
+
+// let arr = [11,62,3,4,25]
+// arr.forEach(function(val){
+// console.log(val + 5)
+// }
+// );
+
+// let arr = [100,200,300]
+// arr.forEach(function(item){
+// console.log(item + 10)
+// }
+// );
+
+// let arr = [100,200,300,"isha"]
+// arr.forEach(function(item){
+// console.log(item + 10)
+// });
+// 
+
+// FILTER
+
+// let arr = [1,2,3,4,5,6,7,8]
+//  let newarr = arr.filter(function(val){
+//     if (val > 3)  return true ;
+// });
+// console.log(newarr);
+
+// REDUCE
+
+// let arr = [1,2,3,4,5,6];
+// let ans = arr.reduce(function(accumulator , val) {
+//     return accumulator + val 
+// } , 0)
+// console.log( ans);
+
+// let arr = [1,2,3,4,5,6];
+// let ans = arr.reduce(function(accumulator , val) {
+//     return accumulator + val 
+// } , 1)
+// console.log( ans);
+
+
+// MAP
+
+// let ar=[1,2,3,4]
+//  let newArr=ar.map((item)=>{
+// return item * 2
+// }
+// );
+// console.log(newArr);
+
+// let ar=[100,200,300,400,500]
+//  let newArr=ar.map((item)=>{
+// return item * 2
+// }
+// );
+// console.log(newArr);
+
+// INDEXOF
+
+// let ar = [10,20,30,40,50,60]
+// console.log(ar .Indexof(10));
+// console.log(ar. lastIndexOf(30));
+
+// let ar = [100,200,"isha"]
+// console.log(ar. lastIndexOf("isha"));
+
+// let ar=[1,2,3,4,5,6,7,8,9,10]
+// console.log(ar. lastIndexOf(8));
+
+
+// INCLUDES (true/false)
+
+// let arr = [100,200,300,400,500]
+// console.log(arr.includes(300));
+
+// let arr = [100,200,300,400,500]
+// console.log(arr.includes(600));
+
+// let arr = [100,200,300, "isha","musuu",400,500]
+// // console.log(arr.includes("ishu"));
+// console.log(arr.includes("musuu"));
+
+
+// QUESTIONS-ANSWER
+
+// let arr = ["apple" , "banana" , "mango"]
+// console.log(arr);
+
+// let colors = ["red", "green", "blue", "yellow"];
+// console.log(colors[colors.length - 1]);
+
+// let arr = [1,2,3,4,5,6,7]
+// let max = Math.max(...arr)
+// console.log(max);
+
+// let arr = [1,2,3,4,5,6,7]
+// let min = Math.min(...arr)
+// console.log(min);
+
+// let arr = [3, 7, 1, 9, 2, 5];
+// let sum = arr.reduce((acc,curr)=> acc + curr,0);
+// console.log(sum);
+
+// let arr=[100,200,300,400]
+// let sum = arr.reduce((acc,curr)=> acc + curr ,0);
+// console.log(sum);
