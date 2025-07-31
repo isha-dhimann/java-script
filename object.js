0a6ee51918af5a6db91bbd56d33c3ceda8fe66af
Isha-dhimann <ishadhiman2681@gmail.com>
2025-07-31 14:41:29 +0530

    31-7-25

---
+++ object.js
@@ -0,0 +1,119 @@
+// ACESS THE OBJECTS IN JS
+
+// let obj = {
+//     name : "isha",
+//     rollno : 23,
+//     age : 23,
+//     city : "delhi"
+// }
+// console.log(obj);
+
+// let data = {
+//     name : "isha",
+//     rollno : 23,
+//     age : 23,
+//     city : "delhi"
+// }
+// console.log(data.name);
+// console.log(data.city);
+// console.log(data.rollno);
+
+// let data = {
+//     name : "isha",
+//     rollno : 23,
+//     age : 23,
+//     city : "delhi"
+// }
+// console.log(data.age);
+//  console.log(delete data.age)
+
+//  data.mobileno= 56789000;
+//  console.log(data.mobileno);
+
+// let obj = {
+//     name : "ishuu",
+//     city : "delhi",
+//     class : "cse"
+// }
+
+
+// EXECUTE
+// console.log(obj);
+
+// CHECKED NAME
+// console.log(obj.name);
+
+// DELETE ELEMENT
+// console.log(delete obj.class);
+// console.log(obj)
+
+// ADD ELEMENT
+// obj.mobilen = 8447927073
+// obj.email = "ishadhiman@gmail.com"
+// console.log(obj);
+
+
+// let eve = {
+//     name : "muskan",
+//     age : 22 , 
+//     city : "hansi-hisar"
+// }
+// console.log(eve.city);
+
+// TO ACESS THE ARRAYS M BHI ....(method-2)
+
+// console.log( eve['city']);
+// console.log( eve['name']);
+// console.log( eve['age']);
+
+// NESTING (ek k ander ek )
+
+// const user = {
+//     name : "isha",
+//     address : {
+//         city : "new-delhi",
+//         pin : 110039 ,
+//         location : {
+//             iat : 23.02,
+//             ing : 56.08,
+//         } , 
+//     } ,
+// };
+//  console.log(user.address.location.ing);
+
+// let student = {
+//   name: "Isha",
+//   age: 22,
+//   course: "B.Tech"
+// };
+// console.log(student);
+// console.log(student.name);
+
+// student.address = "delhi"
+// console.log(student);
+
+// console.log(delete student.age) ;
+
+
+// let student = {
+//   name: "Isha",
+//   age: 22,
+//   course: "B.Tech"
+// };
+
+// Dot notation
+// console.log(student.name);  
+
+// Bracket notation
+// console.log(student["age"]);
+
+// let employee = {
+//   name: "Isha",
+//   address: {
+//     city: "Delhi",
+//     pincode: 110001
+//   }
+// };
+// console.log(employee.address.city); 
+
+
